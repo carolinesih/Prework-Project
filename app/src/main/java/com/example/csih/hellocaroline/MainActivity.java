46006058ebb3cc40dbcfefda69a73696bf398343
Caroline Sih <csih@ucsd.edu>
2018-09-15 23:46:19 -0700

    bonus features enabled

--- app/src/main/java/com/example/csih/hellocaroline/MainActivity.java
+++ app/src/main/java/com/example/csih/hellocaroline/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.csih.hellocaroline;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,42 @@ public class MainActivity extends AppCompatActivity {
                 //Log.i("Caroline", "Button clicked");
                 ((TextView) findViewById(R.id.textView)).setTextColor(
                         getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // grab text that the user input
+                String newText = ((EditText) findViewById(R.id.editText)).getText().toString();
+                // put the text into text view
+                if (TextUtils.isEmpty(newText)) {
+                    ((TextView) findViewById(R.id.textView)).setText("Hello from Caroline!");
+                } else {
+                    ((TextView) findViewById(R.id.textView)).setText(newText);
+                }
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // reset text color
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.white));
+                // reset background color
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorPrimaryDark));
+                // reset text
+                ((TextView) findViewById(R.id.textView)).setText("Hello from Caroline!");
             }
         });
     }
